bb34ab86176a7c008f244200b3ac1539c5ca012b
Mike Daum <mike.daum@coobeeyon.net>
2024-12-02 11:27:21 -0500

    Day 2 episode 1

---
+++ src/bin/02.rs
@@ -0,0 +1,46 @@
+advent_of_code::solution!(2);
+use itertools::Itertools;
+
+fn is_safe(report: &Vec<i32>) -> bool {
+    let differences = report
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
+        .collect_vec();
+    (differences.iter().all(|i| *i > 0) || differences.iter().all(|i| *i < 0))
+        && differences.iter().all(|i| i.abs() >= 1 && i.abs() <= 3)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let parsed = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|i| i.parse::<i32>())
+                .map(Result::unwrap)
+                .collect_vec()
+        })
+        .collect_vec();
+    Some(parsed.into_iter().filter(is_safe).count() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
