22861a87d091b54ce8a743984548ee0f7f652b3d
Mike Daum <mike.daum@coobeeyon.net>
2024-12-02 13:12:54 -0500

    Day 2 episode 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,10 +1,23 @@
 advent_of_code::solution!(2);
 use itertools::Itertools;
 
-fn is_safe(report: &Vec<i32>) -> bool {
-    let differences = report.iter().tuple_windows().map(|(a, b)| b - a);
+fn is_safe_differences(differences: impl Iterator<Item = i32> + Clone) -> bool {
     (differences.clone().all(|i| i > 0) || differences.clone().all(|i| i < 0))
         && differences.clone().all(|i| i.abs() >= 1 && i.abs() <= 3)
+}
+fn is_safe_report(report: &Vec<i32>) -> bool {
+    let differences = report.iter().tuple_windows().map(|(a, b)| b - a);
+    is_safe_differences(differences)
+}
+
+fn is_safe_report_when_dampened(report: &Vec<i32>) -> bool {
+    let n = report.len();
+    is_safe_report(report)
+        || report
+            .into_iter()
+            .cloned()
+            .combinations(n - 1)
+            .any(|v| is_safe_report(&v))
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -17,11 +30,25 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .collect_vec()
         })
         .collect_vec();
-    Some(parsed.into_iter().filter(is_safe).count() as u32)
+    Some(parsed.into_iter().filter(is_safe_report).count() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let parsed = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|i| i.parse::<i32>())
+                .map(Result::unwrap)
+                .collect_vec()
+        })
+        .collect_vec();
+    Some(
+        parsed
+            .into_iter()
+            .filter(is_safe_report_when_dampened)
+            .count() as u32,
+    )
 }
 
 #[cfg(test)]
@@ -37,6 +64,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
