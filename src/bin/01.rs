96ed7e5c6d2953507d4936ba42f6a1ea99ff365c
Mike Daum <mike.daum@coobeeyon.net>
2024-12-02 10:07:16 -0500

    Day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,53 @@
 advent_of_code::solution!(1);
+use itertools::Itertools;
+use std::str::FromStr;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let parsed = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(i32::from_str)
+                .map(Result::unwrap)
+                .collect_vec()
+        })
+        .collect_vec();
+    let transposed = [0, 1]
+        .into_iter()
+        .map(|i| parsed.iter().cloned().map(|a| a[i]).sorted().collect_vec())
+        .collect_vec();
+    let distance = transposed[0]
+        .iter()
+        .zip(transposed[1].iter())
+        .map(|(a, b)| (a - b).abs())
+        .sum::<i32>() as u32;
+    Some(distance)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let parsed = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(u32::from_str)
+                .map(Result::unwrap)
+                .collect_vec()
+        })
+        .collect_vec();
+    let transposed = [0, 1]
+        .into_iter()
+        .map(|i| parsed.iter().cloned().map(|a| a[i]).collect_vec())
+        .collect_vec();
+    return if let [first, second] = &transposed[..] {
+        let counts = second.iter().counts();
+        let distance = first
+            .iter()
+            .map(|i| i * (*counts.get(i).unwrap_or(&0) as u32))
+            .sum();
+        Some(distance)
+    } else {
+        None
+    };
 }
 
 #[cfg(test)]
@@ -15,12 +57,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
