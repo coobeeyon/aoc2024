f91c3322ff5cd19686968277cae98903570f32c8
Mike Daum <mike.daum@coobeeyon.net>
2024-12-03 13:12:49 -0500

    Day 3 episode 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,25 +1,25 @@
 advent_of_code::solution!(3);
 use regex::Regex;
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn string_muls(input: &str) -> u32 {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let sum = input
-        .lines()
-        .map(|line| -> u32 {
-            re.captures_iter(line)
-                .map(|cap| {
-                    let a: u32 = cap[1].parse().unwrap();
-                    let b: u32 = cap[2].parse().unwrap();
-                    a * b
-                })
-                .sum()
+    re.captures_iter(input)
+        .map(|cap| {
+            let a: u32 = cap[1].parse().unwrap();
+            let b: u32 = cap[2].parse().unwrap();
+            a * b
         })
-        .sum();
-    Some(sum)
+        .sum()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(string_muls(input))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let full_input = (input.to_owned() + "do()").replace('\n', "");
+    let re = Regex::new(r"don't\(\).*?do\(\)").unwrap();
+    Some(string_muls(&re.replace_all(&full_input, "")))
 }
 
 #[cfg(test)]
@@ -35,6 +35,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(48));
     }
 }
