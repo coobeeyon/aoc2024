a0064c8428621e350461b0819fef04077bf61402
Mike Daum <mike.daum@coobeeyon.net>
2024-12-05 20:29:43 -0500

    Day 5 Episode 1

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,7 +1,75 @@
+use std::collections::{HashMap, HashSet};
+
+use itertools::{izip, Itertools};
+use regex::Regex;
+
 advent_of_code::solution!(5);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let rules_re = Regex::new(r"(\d+)\|(\d+)").unwrap();
+    let (rules_str, updates_str) = input.split("\n\n").collect_tuple().unwrap();
+    let rules: HashMap<u32, HashSet<u32>> = rules_str
+        .lines()
+        .flat_map(|line| {
+            rules_re
+                .captures_iter(line)
+                .map(|c| c.extract())
+                .map(|(_, [before, after])| {
+                    (
+                        before.parse::<u32>().unwrap(),
+                        after.parse::<u32>().unwrap(),
+                    )
+                })
+        })
+        .sorted()
+        .chunk_by(|e| e.0)
+        .into_iter()
+        .map(|chunk| (chunk.0, chunk.1.map(|e| e.1).collect()))
+        .collect();
+
+    let updates = updates_str
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let processed_rows = updates
+        .iter()
+        .map(|update| {
+            (
+                update[update.len() / 2],
+                izip!(
+                    update.iter(),
+                    (0..update.len())
+                        .map(|i| update[0..i].iter().cloned().collect::<HashSet<u32>>())
+                        .collect_vec(),
+                )
+                .collect_vec(),
+            )
+        })
+        .collect_vec();
+
+    let valid_sum = processed_rows
+        .iter()
+        .map(|(mid, rolling_row)| {
+            if rolling_row.iter().any(|(this_char, seen_chars)| {
+                if let Some(forbidden_chars) = rules.get(this_char) {
+                    forbidden_chars.intersection(seen_chars).count() > 0
+                } else {
+                    false
+                }
+            }) {
+                0
+            } else {
+                *mid
+            }
+        })
+        .sum();
+
+    Some(valid_sum)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +83,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(143));
     }
 
     #[test]
