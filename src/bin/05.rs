652c2d77ac915235c9add12d066577c7756c9b5d
Mike Daum <mike.daum@coobeeyon.net>
2024-12-06 13:06:45 -0500

    Day 5 Episode 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,4 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
 
 use itertools::{izip, Itertools};
 use regex::Regex;
@@ -73,7 +76,56 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let rules_re = Regex::new(r"(\d+)\|(\d+)").unwrap();
+    let (rules_str, updates_str) = input.split("\n\n").collect_tuple().unwrap();
+    let rules: HashSet<(u32, u32)> = rules_str
+        .lines()
+        .flat_map(|line| {
+            rules_re
+                .captures_iter(line)
+                .map(|c| c.extract())
+                .map(|(_, [before, after])| {
+                    (
+                        before.parse::<u32>().unwrap(),
+                        after.parse::<u32>().unwrap(),
+                    )
+                })
+        })
+        .collect();
+
+    let updates = updates_str
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let ret = updates
+        .iter()
+        .filter_map(|update| {
+            let sorted_update = update
+                .iter()
+                .cloned()
+                .sorted_by(|a, b| {
+                    if rules.contains(&(*a, *b)) {
+                        Ordering::Less
+                    } else {
+                        Ordering::Greater
+                    }
+                })
+                .collect_vec();
+            if sorted_update == *update {
+                None
+            } else {
+                Some(sorted_update)
+            }
+        })
+        .map(|update| update[update.len() / 2])
+        .sum();
+
+    Some(ret)
 }
 
 #[cfg(test)]
@@ -89,6 +141,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
