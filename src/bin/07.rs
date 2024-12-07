14e7a23da185406676ab5259fb64fa3f3c64dfe6
Mike Daum <mike.daum@coobeeyon.net>
2024-12-07 18:35:21 -0500

    Day 7 Episode 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -26,49 +26,95 @@ impl Calculation {
     }
 }
 
+trait Operation: Sized {
+    fn operator_combinations(n: usize) -> impl Iterator<Item = Vec<Self>>;
+    fn eval(calc: &Calculation, ops: &[Self]) -> u64;
+}
+
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum Operation {
+pub enum PartOneOperation {
     Multiply,
     Add,
 }
 
-fn operator_combinations(n: usize) -> impl Iterator<Item = Vec<Operation>> {
-    (0..2usize.pow(n as u32)).map(move |i| {
-        format!("{i:0width$b}", width = n)
-            .chars()
-            .map(|c| match c {
-                '0' => Operation::Add,
-                '1' => Operation::Multiply,
-                _ => panic!(),
-            })
-            .collect_vec()
-    })
-}
-
-fn eval(calc: &Calculation, ops: &[Operation]) -> u64 {
-    ops.iter()
-        .zip(calc.args.iter().dropping(1))
-        .fold(calc.args[0], |a, (op, arg)| match op {
-            Operation::Add => a + arg,
-            Operation::Multiply => a * arg,
+impl Operation for PartOneOperation {
+    fn operator_combinations(n: usize) -> impl Iterator<Item = Vec<PartOneOperation>> {
+        (0..2usize.pow(n as u32)).map(move |i| {
+            format!("{i:0width$b}", width = n)
+                .chars()
+                .map(|c| match c {
+                    '0' => PartOneOperation::Add,
+                    '1' => PartOneOperation::Multiply,
+                    _ => panic!(),
+                })
+                .collect_vec()
         })
+    }
+
+    fn eval(calc: &Calculation, ops: &[PartOneOperation]) -> u64 {
+        ops.iter()
+            .zip(calc.args.iter().dropping(1))
+            .fold(calc.args[0], |a, (op, arg)| match op {
+                PartOneOperation::Add => a + arg,
+                PartOneOperation::Multiply => a * arg,
+            })
+    }
 }
 
-pub fn part_one(input: &str) -> Option<u64> {
+fn solve_puzzle<Op>(input: &str) -> Option<u64>
+where
+    Op: Operation,
+{
     let puzzle = Calculation::from_input(input);
     let solution = puzzle
         .iter()
         .filter_map(|calc| {
-            operator_combinations(calc.num_operations())
-                .map(|ops| eval(calc, &ops))
+            Op::operator_combinations(calc.num_operations())
+                .map(|ops| Op::eval(calc, &ops))
                 .find(|v| *v == calc.result)
         })
         .sum();
     Some(solution)
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum PartTwoOperation {
+    Multiply,
+    Add,
+    Concat,
+}
+
+impl Operation for PartTwoOperation {
+    fn operator_combinations(n: usize) -> impl Iterator<Item = Vec<PartTwoOperation>> {
+        (0..n)
+            .map(|_| {
+                [
+                    PartTwoOperation::Add,
+                    PartTwoOperation::Multiply,
+                    PartTwoOperation::Concat,
+                ]
+                .into_iter()
+            })
+            .multi_cartesian_product()
+    }
+
+    fn eval(calc: &Calculation, ops: &[PartTwoOperation]) -> u64 {
+        ops.iter()
+            .zip(calc.args.iter().dropping(1))
+            .fold(calc.args[0], |a, (op, arg)| match op {
+                PartTwoOperation::Add => a + arg,
+                PartTwoOperation::Multiply => a * arg,
+                PartTwoOperation::Concat => (a.to_string() + &arg.to_string()).parse().unwrap(),
+            })
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    solve_puzzle::<PartOneOperation>(input)
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    solve_puzzle::<PartTwoOperation>(input)
 }
 
 #[cfg(test)]
@@ -84,6 +130,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
