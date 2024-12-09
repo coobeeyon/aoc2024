893c08e02b9d9e2c79a4b693cba0f947188e55b2
Mike Daum <mike.daum@coobeeyon.net>
2024-12-09 10:04:20 -0500

    Day 8 WIP

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,6 +1,29 @@
+use itertools::{Chunk, Itertools};
+
 advent_of_code::solution!(8);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let board = input.lines().collect_vec();
+    let width = board[0].len();
+    let height = board.len();
+    let occupied = board
+        .iter()
+        .enumerate()
+        .flat_map(|(row, line)| {
+            line.chars().enumerate().filter_map(move |(col, c)| {
+                if c != '.' {
+                    Some((c, (col, row)))
+                } else {
+                    None
+                }
+            })
+        })
+        .sorted_by(|(ca, _), (cb, _)| ca.cmp(cb))
+        .chunk_by(|(c, _)| *c)
+        .into_iter()
+        .map(|(key, chunk)| (key, chunk.map(|(_, pos)| pos).collect_vec()))
+        .collect_vec();
+    dbg!(occupied);
     None
 }
 
@@ -15,7 +38,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(14));
     }
 
     #[test]
