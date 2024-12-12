23db5b00cfbbef893ad3f6e4a564b5bce5297569
Mike Daum <mike.daum@coobeeyon.net>
2024-12-11 21:47:00 -0500

    Day 11 Episode 1

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,7 +1,36 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(11);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u64> {
+    let nums = input
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect_vec();
+    let num_rocks = itertools::iterate(nums, |nums| {
+        nums.iter()
+            .flat_map(|num| {
+                if *num == 0 {
+                    vec![1]
+                } else {
+                    let num_str = format!("{}", num);
+                    let len = num_str.len();
+                    if num_str.len() % 2 == 0 {
+                        let left = num_str[..len / 2].parse().unwrap();
+                        let right = num_str[len / 2..].parse().unwrap();
+                        vec![left, right]
+                    } else {
+                        vec![num * 2024]
+                    }
+                }
+            })
+            .collect_vec()
+    })
+    .take(26)
+    .last()
+    .unwrap()
+    .len();
+    Some(num_rocks as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +44,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(55312));
     }
 
     #[test]
