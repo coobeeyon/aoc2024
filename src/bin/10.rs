d0ca2830cc7c9a6f5acae243849caabd7fd46749
Mike Daum <mike.daum@coobeeyon.net>
2024-12-11 21:15:08 -0500

    Day 10 Episode 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -71,7 +71,54 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let map = input
+        .lines()
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect_vec())
+        .collect_vec();
+    let trailheads = map
+        .iter()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .filter_map(move |(x, n)| {
+                    if *n == 0 {
+                        Some(vec![(x as i32, y as i32)])
+                    } else {
+                        None
+                    }
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+    let score = (1..=9)
+        .fold(trailheads, |paths, n| {
+            paths
+                .into_iter()
+                .flat_map(|path| {
+                    let (x0, y0) = path[path.len() - 1];
+                    [(x0 - 1, y0), (x0 + 1, y0), (x0, y0 - 1), (x0, y0 + 1)]
+                        .into_iter()
+                        .filter_map(|pos| match pos {
+                            (-1, _) => None,
+                            (_, -1) => None,
+                            (x, y) => map.get(y as usize).and_then(|row| {
+                                row.get(x as usize)
+                                    .and_then(|m| if *m == n { Some(pos) } else { None })
+                            }),
+                        })
+                        .map(|pos| {
+                            let mut path = path.clone();
+                            path.push(pos);
+                            path
+                        })
+                        .collect_vec()
+                })
+                .collect_vec()
+        })
+        .len();
+
+    Some(score as u32)
 }
 
 #[cfg(test)]
@@ -87,6 +134,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(81));
     }
 }
